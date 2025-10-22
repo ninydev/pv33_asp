9c2182d231ea73b616ddd0b0989f406f280b787e
Oleksandr Nykytin <alex_n@alma-soft.com>
2025-10-22 21:00:49 +0300

    Add Validator

---
+++ razor_edition/WebApplication2/WebApplication2/FormsData/ContactForm.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+public class ContactForm
+{
+    [Required(ErrorMessage = "Введите имя")]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "Имя должно быть от 2 до 50 символов")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Введите email")]
+    [EmailAddress(ErrorMessage = "Некорректный адрес электронной почты")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Введите сообщение")]
+    [StringLength(500, MinimumLength = 10, ErrorMessage = "Сообщение должно быть от 10 до 500 символов")]
+    public string Message { get; set; }
+
+    [Range(18, 99, ErrorMessage = "Возраст должен быть от 18 до 99 лет")]
+    public int Age { get; set; }
+}
--- razor_edition/WebApplication2/WebApplication2/Pages/Contact.cshtml.cs
+++ razor_edition/WebApplication2/WebApplication2/Pages/Contact.cshtml.cs
@@ -5,15 +5,68 @@ namespace WebApplication2.Pages;
 
 public class Contact : PageModel
 {
+    
+    [BindProperty]
+    public ContactForm Form { get; set; }
+
     public void OnPost()
     {
-        var name = Request.Form["name"];
-        var email = Request.Form["email"];
-        var message = Request.Form["message"];
+        if (!ModelState.IsValid)
+        {
+            Console.WriteLine("Model is not valid");
+        }
         
-        Console.WriteLine(name + " " + email);
+        ViewData["Name"] = Form.Name;
+        ViewData["Email"] = Form.Email;
+        ViewData["Message"] = Form.Message;
+        ViewData["Age"] = Form.Age;
     }
     
+    // [BindProperty]
+    // public string Name { get; set; }
+    //
+    // [BindProperty]
+    // public string Email { get; set; }
+    //
+    // [BindProperty]
+    // public string Message { get; set; }
+    //
+    // [BindProperty]
+    // public int Age { get; set; }
+    //
+    // public void OnPost()
+    // {
+    //     ViewData["Name"] = Name;
+    //     ViewData["Email"] = Email;
+    //     ViewData["Message"] = Message;
+    //     ViewData["Age"] = Age;
+    // }
+
+    
+    // public void OnPost(string name, int age, string email, string message)
+    // {
+    //     ViewData["Name"] = name;
+    //     ViewData["Email"] = email;
+    //     ViewData["Message"] = message;
+    //     ViewData["Age"] = age;
+    // }
+    
+    
+    // Bad Practice
+    // I read the form data from the request
+    // public void OnPost()
+    // {
+    //     var name = Request.Form["name"];
+    //     var email = Request.Form["email"];
+    //     var message = Request.Form["message"];
+    //     
+    //     ViewData["Name"] = name;
+    //     ViewData["Email"] = email;
+    //     ViewData["Message"] = message;
+    //     
+    //     Console.WriteLine(name + " " + email);
+    // }
+
     /**
      * Код обработки запроса гет будет запущен
      * ДО построения самой страницы и может оработатать запрос
