8972c96323fa68243a0555128ba2f4097188134c
Oleksandr Nykytin <alex_n@alma-soft.com>
2025-10-22 21:14:30 +0300

    DataAnnotations

--- razor_edition/WebApplication2/WebApplication2/FormsData/ContactForm.cs
+++ razor_edition/WebApplication2/WebApplication2/FormsData/ContactForm.cs
@@ -2,15 +2,18 @@ using System.ComponentModel.DataAnnotations;
 
 public class ContactForm
 {
-    [Required(ErrorMessage = "Введите имя")]
+    [Required(ErrorMessage = "Имя обязательное поле")]
+    [Display(Name = "Имя")]
     [StringLength(50, MinimumLength = 2, ErrorMessage = "Имя должно быть от 2 до 50 символов")]
     public string Name { get; set; }
 
-    [Required(ErrorMessage = "Введите email")]
+    [Required(ErrorMessage = "Обязательно Введите email")]
+    [Display(Name = "Email")]   
     [EmailAddress(ErrorMessage = "Некорректный адрес электронной почты")]
     public string Email { get; set; }
 
-    [Required(ErrorMessage = "Введите сообщение")]
+    [Required(ErrorMessage = "Без сообения я не оптавлю сообщение")]
+    [Display(Name = "Введите сообщение")]
     [StringLength(500, MinimumLength = 10, ErrorMessage = "Сообщение должно быть от 10 до 500 символов")]
     public string Message { get; set; }
 
