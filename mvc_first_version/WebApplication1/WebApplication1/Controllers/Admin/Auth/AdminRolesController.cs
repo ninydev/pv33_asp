0e5ad9cdb3e19c415ce409e331aebcb7207814c3
Oleksandr Nykytin <alex_n@alma-soft.com>
2025-11-14 21:12:40 +0200

    Add Admin

--- mvc_first_version/WebApplication1/WebApplication1/Controllers/Admin/Auth/AdminRolesController.cs
+++ mvc_first_version/WebApplication1/WebApplication1/Controllers/Admin/Auth/AdminRolesController.cs
@@ -1,6 +1,15 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
 namespace WebApplication1.Controllers.Admin.Auth;
 
-public class AdminRolesController
+[Authorize(Roles = "Admin")]
+[Route("admin/[controller]")]
+public class AdminRolesController : Controller
 {
-    
+    public IActionResult Index()
+    {
+        return View("~/Views/Admin/AdminRoles/Index.cshtml");
+    }
 }
--- mvc_first_version/WebApplication1/WebApplication1/Controllers/Admin/Auth/AdminUsersController.cs
+++ mvc_first_version/WebApplication1/WebApplication1/Controllers/Admin/Auth/AdminUsersController.cs
@@ -1,6 +1,14 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 namespace WebApplication1.Controllers.Admin.Auth;
 
-public class AdminUsersController
+[Authorize(Roles = "Admin")]
+[Route("admin/[controller]")]
+public class AdminUsersController : Controller
 {
-    
+    public ActionResult Index()
+    {
+        return View();
+    }
 }
