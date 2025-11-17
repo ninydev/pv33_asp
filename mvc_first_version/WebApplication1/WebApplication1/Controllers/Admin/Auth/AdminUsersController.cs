13069eb7065a6e29f8c66d82e7c5c7a0533323c3
Oleksandr Nykytin <alex_n@alma-soft.com>
2025-11-17 20:08:18 +0200

    Add roles

--- mvc_first_version/WebApplication1/WebApplication1/Controllers/Admin/Auth/AdminUsersController.cs
+++ mvc_first_version/WebApplication1/WebApplication1/Controllers/Admin/Auth/AdminUsersController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Entities;
+using System.Threading.Tasks;
 
 namespace WebApplication1.Controllers.Admin.Auth;
 
@@ -17,7 +18,8 @@ public class AdminUsersController : Controller
         this.userManager = userManager;
     }
     
-    public ActionResult Index(int page = 1, int pageSize = 10, string? search = null)
+    public async Task<ActionResult> Index(int page = 1, int pageSize = 10, 
+        string? search = null)
     {
         if (page < 1) page = 1;
         if (pageSize < 1) pageSize = 1;
@@ -38,10 +40,19 @@ public class AdminUsersController : Controller
             .Take(pageSize)
             .ToList();
 
+        // Собираем роли только для пользователей на текущей странице
+        var rolesByUser = new Dictionary<string, IList<string>>();
+        foreach (var u in users)
+        {
+            var roles = await userManager.GetRolesAsync(u);
+            rolesByUser[u.Id] = roles;
+        }
+
         ViewData["Page"] = page;
         ViewData["PageSize"] = pageSize;
         ViewData["TotalItems"] = totalItems;
         ViewData["Search"] = search ?? string.Empty;
+        ViewBag.UserRoles = rolesByUser;
         return View("~/Views/Admin/AdminUsers/Index.cshtml", users);
     }
 }
