8cdb5f605c960bc78758097c119f96426f627a22
Oleksandr Nykytin <keeper@ninydev.com>
2025-12-01 20:30:28 +0200

    Sse

--- mvc_first_version/WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ mvc_first_version/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -1,20 +1,24 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Entities;
+using WebApplication1.Sse;
 
 namespace WebApplication1.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly SseService _sseService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, SseService sseService)
     {
         _logger = logger;
+        _sseService = sseService;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        await _sseService.SendToAllAsync("Hello from server!");
         return View();
     }
 
